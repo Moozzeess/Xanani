5a18c7f7c1faff984dfde39dbf35f2ad9b2e12d9
Luis <ln70922@gmail.com>
2026-05-15 00:50:34 -0700

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,8 @@ app.use(morgan(
 ));
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(compression()); // Optimizacion de payload
