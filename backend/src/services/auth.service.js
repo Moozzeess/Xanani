3bafefdf15918c266cca3a8115b85a0d39aba562
EGu5 <147118136+EGu5@users.noreply.github.com>
2026-05-20 23:14:53 -0600

    Solución de conflicto en auth.service.js

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -261,11 +261,7 @@ module.exports = {
   register,
   login,
   verifyEmail,
-<<<<<<< HEAD
-  resendVerificationEmail,
-=======
   resendVerification,
->>>>>>> 9a832ee (cambios de reenvio)
   forgotPassword,
   resetPassword
 };
